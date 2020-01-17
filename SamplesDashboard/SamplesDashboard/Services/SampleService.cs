9e93d73f9a2d465efa2b12045dafcbd72249869e
Maggie Kimani <maggiekim42@gmail.com>
2020-01-17 16:25:41 +0300

    Create SamplesAPI to retrieve data from Github API

---
+++ SamplesDashboard/SamplesDashboard/Services/SampleService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+//using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Octokit.GraphQL;
+using System.Collections.Generic;
+using SamplesDashboard.Models;
+
+namespace SamplesDashboard.Services
+{
+    public class SampleServices
+    {
+        public static async Task<List<Repo>> GetSamples()
+        {
+            var productInformation = new ProductHeaderValue("Octokit.GraphQL", "0.1.4-beta");
+            var connection = new Connection(productInformation, "305098146e3c47ef05c78244e3c1953516bd26d9");
+
+            var query = new Query()
+                .RepositoryOwner("microsoftgraph")
+                .Repositories(first: 50)
+                .Nodes
+                .Select(r => new Repo {
+                    Name = r.Name,
+                    Owner = r.Owner,
+
+
+                });
+             
+            //run query 
+            var result = await connection.Run(query);
+
+            return result.ToList();
+        }
+    }
+}
