9c5010fadcfb19fa6781a3d566f393d8d5f205f6
Elinor <ekaguongo@gmail.com>
2020-01-20 12:54:42 +0300

    Create samples dashboard data table

---
+++ SamplesDashboard/SamplesDashboard/ClientApp/src/components/Cell.js
@@ -0,0 +1,19 @@
+﻿import * as React from 'react';
+
+export default function Cell({
+    content,
+    header,
+}) {
+
+    const cellMarkup = header ? (
+        <th className="Cell Cell-header">
+            {content}
+        </th>
+    ) : (
+            <td className="Cell">
+                {content}
+            </td>
+        );
+
+    return (cellMarkup);
+}
---
+++ SamplesDashboard/SamplesDashboard/ClientApp/src/components/DataTable.js
@@ -0,0 +1,55 @@
+﻿import * as React from 'react';
+import Cell from './Cell';
+
+export default class DataTable extends React.Component {
+    renderHeadingRow = (_cell, cellIndex) => {
+        const { headings } = this.props;
+
+        return (
+            <Cell
+                key={`heading-${cellIndex}`}
+                content={headings[cellIndex]}
+                header={true}
+            />
+        )
+    };
+
+    renderRow = (_row, rowIndex) => {
+        const { rows } = this.props;
+
+        return (
+            <tr key={`row-${rowIndex}`}>
+                {rows[rowIndex].map((_cell, cellIndex) => {
+                    return (
+                        <Cell
+                            key={`${rowIndex}-${cellIndex}`}
+                            content={rows[rowIndex][cellIndex]}
+                        />
+                    )
+                })}
+            </tr>
+        )
+    };
+
+    render() {
+        const { headings, rows } = this.props;
+
+        this.renderHeadingRow = this.renderHeadingRow.bind(this);
+        this.renderRow = this.renderRow.bind(this);
+
+        const theadMarkup = (
+            <tr key="heading">
+                {headings.map(this.renderHeadingRow)}
+            </tr>
+        );
+
+        const tbodyMarkup = rows.map(this.renderRow);
+
+        return (
+            <table className="Table">
+                <thead>{theadMarkup}</thead>
+                <tbody>{tbodyMarkup}</tbody>
+            </table>
+        );
+    }
+}
