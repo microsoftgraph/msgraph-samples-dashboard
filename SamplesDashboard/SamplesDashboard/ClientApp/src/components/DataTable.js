e8ca41146feb2650f7a77a003c29e570d26156e3
Elinor <ekaguongo@gmail.com>
2020-01-21 13:17:11 +0300

    Display data from Github Api on data table

--- SamplesDashboard/SamplesDashboard/ClientApp/src/components/DataTable.js
+++ SamplesDashboard/SamplesDashboard/ClientApp/src/components/DataTable.js
@@ -1,55 +1,67 @@
-﻿import * as React from 'react';
-import Cell from './Cell';
+﻿import React, { Component } from 'react';
 
-export default class DataTable extends React.Component {
-    renderHeadingRow = (_cell, cellIndex) => {
-        const { headings } = this.props;
+export default class DataTable extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            tableData: []
+        }
+    }
 
-        return (
-            <Cell
-                key={`heading-${cellIndex}`}
-                content={headings[cellIndex]}
-                header={true}
-            />
+    componentDidMount = () => {
+        this.fetchData();
+    }
+
+    //fetching the data from the api
+    fetchData = async () => {
+        const response = await fetch('api/Samples');
+        const data = await response.json();
+        this.setState(
+            {
+                tableData: data
+            });
+    }
+    //displaying sample data in the appropriate columns
+    renderData() {
+        return this.state.tableData.map(sample =>
+            (
+                <tr>
+                    <td>{sample.name}</td>
+                    <td>{sample.owner}</td>
+                    <td>{sample.status}</td>
+                    <td>{sample.language}</td>
+                    <td>{sample.pullRequests}</td>
+                    <td>{sample.issues}</td>
+                    <td>{sample.stars}</td>
+                    <td>{sample.featureArea}</td>
+                    <td>{sample.securityAlerts}</td>
+                </tr>
+            ),
         )
-    };
-
-    renderRow = (_row, rowIndex) => {
-        const { rows } = this.props;
-
-        return (
-            <tr key={`row-${rowIndex}`}>
-                {rows[rowIndex].map((_cell, cellIndex) => {
-                    return (
-                        <Cell
-                            key={`${rowIndex}-${cellIndex}`}
-                            content={rows[rowIndex][cellIndex]}
-                        />
-                    )
-                })}
-            </tr>
-        )
-    };
+    }
 
     render() {
-        const { headings, rows } = this.props;
-
-        this.renderHeadingRow = this.renderHeadingRow.bind(this);
-        this.renderRow = this.renderRow.bind(this);
-
-        const theadMarkup = (
-            <tr key="heading">
-                {headings.map(this.renderHeadingRow)}
-            </tr>
-        );
-
-        const tbodyMarkup = rows.map(this.renderRow);
-
         return (
-            <table className="Table">
-                <thead>{theadMarkup}</thead>
-                <tbody>{tbodyMarkup}</tbody>
-            </table>
+            <div>
+                <table className="Table">
+                    <thead>
+                        <tr>
+                            <th>Sample Name</th>
+                            <th>Owner</th>
+                            <th>Status</th>
+                            <th>Language</th>
+                            <th>Open Pull Requests</th>
+                            <th>Open Issues</th>
+                            <th>Stars</th>
+                            <th>Feature Area</th>
+                            <th>Security Alerts</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {this.renderData()}                        
+                    </tbody>          
+                </table>
+            </div>
         );
     }
 }
--- SamplesDashboard/SamplesDashboard/ClientApp/src/components/Home.js
+++ SamplesDashboard/SamplesDashboard/ClientApp/src/components/Home.js
@@ -1,64 +1,15 @@
 import React, { Component } from 'react';
 import DataTable from './DataTable';
 
-class Home extends Component {
+class Home extends Component {          
     render() {
-
-        const headings = [
-            'Sample name',
-            'Owner',
-            'Status',
-            'Language',
-            'Open Pull Requests',
-            'Open Issues',
-            'Stars',
-            'Feature Area',
-            'Security Alerts',
-        ];
-
-        const rows = [
-            [
-                'Sample 1',
-                '@Bettirose',
-                'Urgent Update',
-                'C#',
-                4,
-                10,
-                21,
-                'InTune',
-                'None',
-            ],
-            [
-                'Sample 2',
-                '@MaggieKim1',
-                'Update',
-                'JavaScript',
-                1,
-                3,
-                15,
-                'Outlook',
-                2,
-            ],
-            [
-                'Sample 3',
-                '@ElinorW',
-                'Up-to Date',
-                'PHP',
-                9,
-                2,
-                12,
-                'Teams',
-                4,
-            ],
-        ];
-
         return (
             <div>
                 <h1> Samples Dashboard </h1>
                 <br/>
                 <h2> List of Samples </h2>
-                <br/>
-                <DataTable headings={headings} rows={rows} />
+                <br />
+                <DataTable  />
             </div>
         );
     }
