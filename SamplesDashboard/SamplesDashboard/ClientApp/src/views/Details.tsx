ab4a7532a56c077796c18586d0115a9387a506b5
Elinor <ekaguongo@gmail.com>
2020-02-14 17:08:39 +0300

    Add the sample details columns

--- SamplesDashboard/SamplesDashboard/ClientApp/src/views/Details.tsx
+++ SamplesDashboard/SamplesDashboard/ClientApp/src/views/Details.tsx
@@ -1,14 +1,33 @@
-﻿import { Fabric } from 'office-ui-fabric-react';
+﻿import { DetailsList, DetailsListLayoutMode, Fabric, IColumn, 
+    initializeIcons, SelectionMode, ShimmeredDetailsList } from 'office-ui-fabric-react';
 import * as React from 'react';
 
 import PageTitle from '../components/layout/PageTitle';
+import { ISampleItem } from '../types/samples';
+
+initializeIcons();
 
 export default class SampleDependencies extends React.Component<any, any> {
+    private allItems: ISampleItem[];
 
     constructor(props: {}) {
         super(props);
 
+        this.allItems = [];
+        const columns: IColumn[] = [
+            { key: 'library', name: 'Library', fieldName: 'library', minWidth: 200, maxWidth: 300, isRowHeader: true, 
+                isResizable: true, isSorted: true, isSortedDescending: false },
+            { key: 'sampleVersion', name: 'Sample Version', fieldName: 'sampleVersion', minWidth: 200, maxWidth: 300, 
+                isResizable: true },
+            { key: 'currentVersion', name: 'Current Version', fieldName: 'currentVersion', minWidth: 200, 
+                maxWidth: 300, isResizable: true },
+            { key: 'status', name: 'Status', fieldName: 'status', minWidth: 200, maxWidth: 300, 
+                isResizable: true },         
+        ];
+
         this.state = {
+            columns,
+            items: this.allItems,
             repositoryDetails: {},
         };
     }
@@ -29,16 +48,23 @@ export default class SampleDependencies extends React.Component<any, any> {
       }
 
     public render(): JSX.Element {
-        const { repositoryDetails } = this.state;
+        const { columns, items, repositoryDetails } = this.state;
 
         return (
             <Fabric>
                 <div>
-                    <PageTitle title={repositoryDetails.name} />    
+                    <PageTitle title={repositoryDetails.name} />
+                    <ShimmeredDetailsList
+                        items={items}
+                        columns={columns}
+                        selectionMode={SelectionMode.none}
+                        layoutMode={DetailsListLayoutMode.justified}
+                        isHeaderVisible={true}
+                    />
                 </div>
             </Fabric>
         );
     }
-
-
 }
+
+
