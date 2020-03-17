930ffadd5c76261ab59079b1029030d01828fd33
Maggie Kimani <maggiekim42@gmail.com>
2020-03-17 13:19:22 +0300

    Creating a new sdk controller to manage sdk endpoints

---
+++ SamplesDashboard/SamplesDashboard/Controllers/SdksController.cs
@@ -0,0 +1,52 @@
+﻿// ------------------------------------------------------------------------------
+//  Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.  See License in the project root for license information.
+// ------------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Configuration;
+using SamplesDashboard.Services;
+
+namespace SamplesDashboard.Controllers
+{
+    [ApiController]
+    public class SdksController : Controller
+    {
+        private readonly SampleService _sampleService;
+        private IMemoryCache _cache;
+        private readonly IConfiguration _config;
+
+        public SdksController(SampleService sampleService, IMemoryCache memoryCache, IConfiguration config)
+        {
+            _sampleService = sampleService;
+            _cache = memoryCache;
+            _config = config;
+        }
+
+        [Produces("application/json")]
+        [Route("api/[controller]")]
+        [HttpGet]
+        public async Task<IActionResult> GetSdksListAsync()
+        {
+            string name = " sdk";
+            List<Node> sdkList;
+
+            if (!_cache.TryGetValue("sdks", out sdkList))
+            {
+                sdkList = await _sampleService.GetSamples(name);
+
+                //Read timeout from config file 
+                var cacheEntryOptions = new MemoryCacheEntryOptions().SetAbsoluteExpiration(TimeSpan.FromSeconds(_config.GetValue<double>("timeout")));
+
+                // Save data in cache.
+                _cache.Set("sdks", sdkList, cacheEntryOptions);
+            }
+
+            return Ok(sdkList);
+        }
+
+    }
+}
