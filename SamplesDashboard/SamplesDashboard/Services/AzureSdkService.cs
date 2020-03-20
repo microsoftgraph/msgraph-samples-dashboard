6427b4df0077f71202f33081c2cfc309a51aedaa
Maggie Kimani <maggiekim42@gmail.com>
2020-03-20 11:27:19 +0300

    caching the azure sdk versions to avoid many network calls

--- SamplesDashboard/SamplesDashboard/Services/AzureSdkService.cs
+++ SamplesDashboard/SamplesDashboard/Services/AzureSdkService.cs
@@ -1,44 +1,65 @@
-﻿using System;
+﻿// ------------------------------------------------------------------------------
+//  Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.  See License in the project root for license information.
+// ------------------------------------------------------------------------------
+
+using System;
 using System.Collections.Generic;
 using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
 using System.Xml;
-using System.Xml.Linq;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Configuration;
 
 namespace SamplesDashboard.Services
 {
+    /// <summary>
+    /// This class contains methods for handling azure sdk versions
+    /// </summary>
     public class AzureSdkService
     {
         private readonly IHttpClientFactory _clientFactory;
-        public AzureSdkService(IHttpClientFactory clientFactory)
+        private IMemoryCache _cache;
+        private readonly IConfiguration _config;
+
+        public AzureSdkService(IHttpClientFactory clientFactory, IMemoryCache cache)
         {
             _clientFactory = clientFactory;
-            
-        }
+            _cache = cache;
+        } 
+
+        /// <summary>
+        /// Downloads and parses an xml file to get a dictionary of azure sdk versions
+        /// </summary>
+        /// <returns> a dictionary of packages</returns>
         public async Task<Dictionary<string, string>> FetchAzureSdkVersions()
         {
-            var httpClient = _clientFactory.CreateClient();
-
-            HttpResponseMessage responseMessage = await httpClient.GetAsync("https://raw.githubusercontent.com/Azure/azure-sdk-for-net/master/eng/Packages.Data.props");
-
-            if (!responseMessage.IsSuccessStatusCode)
-            {
-                return null;
-            }
-            string fileContents = await responseMessage.Content.ReadAsStringAsync();
-            XmlDocument xml = new XmlDocument();
-            xml.LoadXml(fileContents);
-
-            XmlNodeList data = xml.GetElementsByTagName("PackageReference");
             Dictionary<string, string> packages = new Dictionary<string, string>();
 
-            foreach (XmlNode node in data)
+            if (!_cache.TryGetValue("azureSdkVersions", out packages))
             {
-                if (!packages.ContainsKey(node.Attributes["Update"].Value))
+                var httpClient = _clientFactory.CreateClient();
+
+                HttpResponseMessage responseMessage = await httpClient.GetAsync("https://raw.githubusercontent.com/Azure/azure-sdk-for-net/master/eng/Packages.Data.props");
+
+                if (!responseMessage.IsSuccessStatusCode)
                 {
-                    packages.Add(node.Attributes["Update"].Value, node.Attributes["Version"].Value);
+                    return null;
                 }
+                string fileContents = await responseMessage.Content.ReadAsStringAsync();
+                XmlDocument xml = new XmlDocument();
+                xml.LoadXml(fileContents);
+
+                XmlNodeList data = xml.GetElementsByTagName("PackageReference");
+
+                foreach (XmlNode node in data)
+                {
+                    if (!packages.ContainsKey(node.Attributes["Update"].Value))
+                    {
+                        packages.Add(node.Attributes["Update"].Value, node.Attributes["Version"].Value);
+                    }
+                }
+                var cacheEntryOptions = new MemoryCacheEntryOptions().SetAbsoluteExpiration(TimeSpan.FromSeconds(_config.GetValue<double>(Constants.Timeout)));
+                _cache.Set("azureSdkVersions", packages, cacheEntryOptions);
             }
 
             return packages;
