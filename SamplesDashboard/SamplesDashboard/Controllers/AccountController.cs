bd1109ea79a0938fe0e74144079d1630e5a885ef
Maggie Kimani <maggiekim42@gmail.com>
2020-03-25 16:58:12 +0300

    Adding authentication using work account

---
+++ SamplesDashboard/SamplesDashboard/Controllers/AccountController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SamplesDashboard.Controllers
+{
+    [Route("[controller]/[action]")]
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public IActionResult SignIn()
+        {
+            var redirectUrl = Url.Page("/Index");
+            return Challenge(
+                new AuthenticationProperties { RedirectUri = redirectUrl },
+                OpenIdConnectDefaults.AuthenticationScheme
+            );
+        }
+
+        [HttpGet]
+        public IActionResult SignOut()
+        {
+            var callbackUrl = Url.Page("/Account/SignedOut", pageHandler: null, values: null, protocol: Request.Scheme);
+            return SignOut(
+                new AuthenticationProperties { RedirectUri = callbackUrl },
+                CookieAuthenticationDefaults.AuthenticationScheme, OpenIdConnectDefaults.AuthenticationScheme
+            );
+        }
+    }
+}
--- SamplesDashboard/SamplesDashboard/Controllers/RepositoriesController.cs
+++ SamplesDashboard/SamplesDashboard/Controllers/RepositoriesController.cs
@@ -14,6 +14,7 @@ using Microsoft.Extensions.Logging;
 namespace SamplesDashboard.Controllers
 {
     [ApiController]
+    [Microsoft.AspNetCore.Authorization.Authorize]
     public class RepositoriesController : Controller
     {
         private readonly RepositoriesService _repositoriesService;
