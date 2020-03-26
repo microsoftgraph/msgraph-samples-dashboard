c41c0ec868b142c4a111a86ab88fe8828d8447c8
Elinor <ekaguongo@gmail.com>
2020-03-26 12:54:41 +0300

    Remove unused import

--- SamplesDashboard/SamplesDashboard/ClientApp/src/views/Home.tsx
+++ SamplesDashboard/SamplesDashboard/ClientApp/src/views/Home.tsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Repositories from './Repositories';
-import { PivotItem, Pivot, PivotLinkSize, PivotLinkFormat, FontSizes } from 'office-ui-fabric-react';
+import { PivotItem, Pivot, PivotLinkSize, FontSizes } from 'office-ui-fabric-react';
 
 
 class Home extends Component {          
