b023970dc93b79f096007c8e56b66a0646e64c3b
Elinor <ekaguongo@gmail.com>
2020-04-30 18:11:58 +0300

    Style status statistics

--- SamplesDashboard/SamplesDashboard/ClientApp/src/components/layout/PageTitle.tsx
+++ SamplesDashboard/SamplesDashboard/ClientApp/src/components/layout/PageTitle.tsx
@@ -6,7 +6,7 @@ export default function PageTitle(props: any) {
     const classNames = mergeStyleSets({
         pageTitle: {
             fontSize: FontSizes.large,
-            padding: '5px'
+            padding: '5px 5px 0px 10px'            
         }
     });
     
