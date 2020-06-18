da21cbd981a41e526574a096b238fd54772973e9
Maggie Kimani <maggiekim42@gmail.com>
2020-06-19 00:23:02 +0300

    Adjusting card height and aligning text at the center

--- SamplesDashboard/SamplesDashboard/ClientApp/src/views/details/Details.Styles.tsx
+++ SamplesDashboard/SamplesDashboard/ClientApp/src/views/details/Details.Styles.tsx
@@ -29,7 +29,7 @@ export const linkClass = mergeStyles({
 export const classNames = mergeStyleSets({
     wrapper: {
         background: '#fff',
-        height: '57vh',
+        height: '55vh',
         position: 'relative',
         display: 'flex',
         flexWrap: 'wrap',
