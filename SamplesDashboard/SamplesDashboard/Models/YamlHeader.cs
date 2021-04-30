4ea6aa94d2c325ceac85ebf12b40cb3dd83f05f9
Jason Johnston <jasonjoh@microsoft.com>
2021-04-30 11:46:45 -0400

    Refactored YAML header processing
    - Moved YAML processing into the `YamlHeader` class and simplified code in `RepositoriesService` around using the header.
    - YAML will now be read from 3 places (if present): README, repo-name.yml, and devx.yml and combined. In case the same setting is present in more than one of these places, values are overwritten with devx.yml taking precedence over repo-name.yml taking precedence over README

--- SamplesDashboard/SamplesDashboard/Models/YamlHeader.cs
+++ SamplesDashboard/SamplesDashboard/Models/YamlHeader.cs
@@ -2,6 +2,13 @@
 //  Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.  See License in the project root for license information.
 // ------------------------------------------------------------------------------
 
+using System;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using YamlDotNet.Serialization;
+using YamlDotNet.Serialization.NamingConventions;
+
 namespace SamplesDashboard.Models
 {
     public class YamlHeaderExtensions
@@ -14,5 +21,121 @@ namespace SamplesDashboard.Models
         public YamlHeaderExtensions Extensions { get; set; }
         public string DependencyFile { get; set; }
         public bool NoDependencies { get; set; }
+        internal bool NoDependenciesWasSpecified { get; set; }
+
+        public void MergeWith(YamlHeader mergeTarget)
+        {
+            if (mergeTarget == null) return;
+
+            Languages = Languages ?? mergeTarget.Languages;
+            Extensions = Extensions ?? mergeTarget.Extensions;
+            DependencyFile = DependencyFile ?? mergeTarget.DependencyFile;
+            NoDependencies = NoDependenciesWasSpecified ? NoDependencies : NoDependencies || mergeTarget.NoDependencies;
+        }
+
+        public static YamlHeader FromString(string headerContent)
+        {
+            var deserializer = new DeserializerBuilder()
+                    .WithNamingConvention(CamelCaseNamingConvention.Instance)
+                    .IgnoreUnmatchedProperties()
+                    .Build();
+            var header = deserializer.Deserialize<YamlHeader>(headerContent);
+
+            if (headerContent.Contains("noDependencies"))
+            {
+                header.NoDependenciesWasSpecified = true;
+            }
+
+            return header;
+        }
+
+        public static async Task<YamlHeader> GetFromRepo(HttpClient httpClient,
+                                                         string repoName,
+                                                         string branch)
+        {
+            var currentHeader = await GetFromReadme(httpClient, repoName, branch);
+            var repoNameFileHeader = await GetFromFile(httpClient, repoName, branch, $"{repoName}.yml");
+            if (repoNameFileHeader != null)
+            {
+                repoNameFileHeader.MergeWith(currentHeader);
+                currentHeader = repoNameFileHeader;
+            }
+
+            var devxFileHeader = await GetFromFile(httpClient, repoName, branch, "devx.yml");
+            if (devxFileHeader != null)
+            {
+                devxFileHeader.MergeWith(currentHeader);
+                currentHeader = devxFileHeader;
+            }
+
+            return currentHeader;
+
+        }
+
+        public string LanguagesString 
+        {
+            get
+            {
+                return Languages == null ? "" : string.Join(",", Languages);
+            }
+        }
+
+        public string ServicesString
+        {
+            get
+            {
+                return (Extensions == null || Extensions.Services == null) ? 
+                    "" : string.Join(",", Extensions.Services);
+            }
+        }
+
+        private static async Task<YamlHeader> GetFromReadme(HttpClient httpClient,
+                                                            string repoName,
+                                                            string branch)
+        {
+            var responseMessage = await httpClient.GetAsync(
+                $"https://raw.githubusercontent.com/microsoftgraph/{repoName}/{branch}/README.md");
+
+            if (responseMessage.StatusCode.ToString().Equals("NotFound"))
+                responseMessage = await httpClient.GetAsync(
+                    $"https://raw.githubusercontent.com/microsoftgraph/{repoName}/{branch}/Readme.md");
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var fileContents = await responseMessage.Content.ReadAsStringAsync();
+                var stringSeparator = new string[] { "\r\n---\r\n", "\n---\n" };
+                var parts = fileContents.Split(stringSeparator, StringSplitOptions.RemoveEmptyEntries);
+
+                //we have a valid header between ---
+                if (parts.Length > 1)
+                {
+                    var header = parts[0];
+                    if (header.StartsWith("---"))
+                    {
+                        header = header.Substring(3);
+                    }
+                    return FromString(header);
+                }
+            }
+
+            return null;
+        }
+
+        private static async Task<YamlHeader> GetFromFile(HttpClient httpClient,
+                                                          string repoName,
+                                                          string branch,
+                                                          string filePath)
+        {
+            var responseMessage = await httpClient.GetAsync(
+                $"https://raw.githubusercontent.com/microsoftgraph/{repoName}/{branch}/{filePath}");
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var fileContents = await responseMessage.Content.ReadAsStringAsync();
+                return FromString(fileContents);
+            }
+
+            return null;
+        }
     }
 }
---
+++ SamplesDashboard/SamplesDashboardTests/YamlHeaderTests.cs
@@ -0,0 +1,161 @@
+﻿// ------------------------------------------------------------------------------
+//  Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.  See License in the project root for license information.
+// ------------------------------------------------------------------------------
+
+using Microsoft.Extensions.DependencyInjection;
+using SamplesDashboard.Models;
+using System.Net.Http;
+using System.Threading.Tasks;
+using SamplesDashboardTests.Factories;
+using SamplesDashboardTests.MessageHandlers;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace SamplesDashboardTests
+{
+    public class YamlHeaderTests : IClassFixture<BaseWebApplicationFactory<TestStartup>>
+    {
+        private HttpClient _httpClient;
+
+        public YamlHeaderTests(BaseWebApplicationFactory<TestStartup> applicationFactory, ITestOutputHelper helper)
+        {
+            var factory = applicationFactory.Services.GetRequiredService<IHttpClientFactory>();
+            _httpClient = factory.CreateClient();
+        }
+
+        [Fact]
+        public void ShouldParseYamlContent()
+        {
+            // Arrange
+            var yamlContent =
+@"
+languages:
+- java
+extensions:
+    services:
+    - Outlook
+    - OneDrive
+    - Teams
+dependencyFile: demo/GraphTutorial/app/build.gradle
+noDependencies: false";
+
+            // Act
+            var header = YamlHeader.FromString(yamlContent);
+
+            // Assert
+            Assert.NotNull(header);
+            Assert.Equal("demo/GraphTutorial/app/build.gradle", header.DependencyFile);
+            Assert.NotNull(header.Languages);
+            Assert.Single(header.Languages);
+            Assert.Contains("java", header.Languages);
+            Assert.NotNull(header.Extensions);
+            Assert.NotNull(header.Extensions.Services);
+            Assert.Equal(3, header.Extensions.Services.Length);
+            Assert.Contains("Outlook", header.Extensions.Services);
+            Assert.False(header.NoDependencies);
+            Assert.True(header.NoDependenciesWasSpecified);
+        }
+
+        [Fact]
+        public void ShouldMergeButNotOverwrite()
+        {
+            // Arrange
+            var mergeHeaderContent =
+@"
+languages:
+- csharp
+extensions:
+    services:
+    - Outlook
+    - OneDrive
+    - Teams
+noDependencies: true";
+
+            var mergeIntoHeaderContent =
+@"
+languages:
+- java
+dependencyFile: demo/GraphTutorial/app/build.gradle
+noDependencies: false";
+
+            // Act
+            var mergeHeader = YamlHeader.FromString(mergeHeaderContent);
+            var mergeIntoHeader = YamlHeader.FromString(mergeIntoHeaderContent);
+            mergeIntoHeader.MergeWith(mergeHeader);
+
+            // Assert
+            Assert.Single(mergeIntoHeader.Languages);
+            Assert.Contains("java", mergeIntoHeader.Languages);
+            Assert.DoesNotContain("csharp", mergeIntoHeader.Languages);
+            Assert.NotNull(mergeIntoHeader.Extensions);
+            Assert.NotNull(mergeIntoHeader.Extensions.Services);
+            Assert.Equal(3, mergeIntoHeader.Extensions.Services.Length);
+            Assert.Contains("Outlook", mergeIntoHeader.Extensions.Services);
+            Assert.False(mergeIntoHeader.NoDependencies);
+        }
+
+        [Fact]
+        public async Task ShouldGetHeaderFromRepo()
+        {
+            // Arrange
+            var repoName = "msgraph-sdk-java";
+            var branch = "dev";
+
+            // Act
+            var header = await YamlHeader.GetFromRepo(_httpClient, repoName, branch);
+
+            // Assert
+            Assert.NotNull(header);
+        }
+
+        [Fact]
+        public async Task ShouldCombineMultipleSources()
+        {
+            // Arrange
+            var readmeHeader =
+@"---
+languages:
+- java
+extensions:
+    services:
+    - Outlook
+    - OneDrive
+    - Teams
+---
+
+# README TITLE
+
+Some text";
+
+            var repoNameHeader =
+@"
+extensions:
+    services:
+    - Outlook
+    - OneDrive
+dependencyFile: demo/GraphTutorial/app/build.gradle";
+
+            var devxHeader =
+@"
+dependencyFile: demo/GraphTutorial/app/dependencies.gradle";
+
+            var contentHandler = new StringContentHandler(new string[] { readmeHeader, repoNameHeader, devxHeader });
+            var httpClient = new HttpClient(contentHandler);
+
+            // Act
+            var header = await YamlHeader.GetFromRepo(httpClient, "repo", "branch");
+
+            // Assert
+            Assert.Single(header.Languages);
+            Assert.Contains("java", header.Languages);
+            Assert.NotNull(header.Extensions);
+            Assert.NotNull(header.Extensions.Services);
+            Assert.Equal(2, header.Extensions.Services.Length);
+            Assert.Contains("Outlook", header.Extensions.Services);
+            Assert.DoesNotContain("Teams", header.Extensions.Services);
+            Assert.NotNull(header.DependencyFile);
+            Assert.Equal("demo/GraphTutorial/app/dependencies.gradle", header.DependencyFile);
+            Assert.False(header.NoDependencies);
+        }
+    }
+}
