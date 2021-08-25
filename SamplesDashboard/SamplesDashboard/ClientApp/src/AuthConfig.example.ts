67abd41fc1f1719f39eb88db278e2a17cf097626
Jason Johnston <jasonjoh@microsoft.com>
2021-08-25 10:21:54 -0400

    Moved app ID and tenant ID to .env file
    This should play nicer with pipeline validation

--- SamplesDashboard/SamplesDashboard/ClientApp/src/AuthConfig.example.ts
+++
@@ -1,8 +0,0 @@
-// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT License.
-
-export const authConfig = {
-  clientId: 'CLIENT_ID_FROM_AZURE_PORTAL',
-  authority: 'https://login.microsoftonline.com/TENANT_ID_FROM_AZURE_PORTAL',
-  redirectUri: 'DASHBOARD_URL',
-};
